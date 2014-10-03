436eb2699dab059e1ae02b5094e22184b76a45b3
Jason Haines <github@jhaines.name>
2014-10-03 11:33:30 +0200

    fastrweb/metadata.rb for v0.1.1

---
+++ fastrweb/metadata.rb
@@ -0,0 +1,8 @@
+name             'fastrweb'
+maintainer       'geldberg'
+maintainer_email 'chef@geldberg.com'
+license          'All rights reserved'
+description      'Installs/Configures fastrweb'
+long_description 'Installs/Configures fastrweb'
+version          '0.1.1'
+
